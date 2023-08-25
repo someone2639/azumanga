e222ea818d98f787521f4feed4dabe5d7d4577ce
someone2639 <someone2639@gmail.com>
2023-08-25 12:52:57 -0400

    add asm-differ

--- tools/asm-differ/diff_settings.py
+++ tools/asm-differ/diff_settings.py
@@ -1,7 +1,7 @@
 def apply(config, args):
-    config["baseimg"] = "target.bin"
-    config["myimg"] = "source.bin"
-    config["mapfile"] = "build.map"
+    config["baseimg"] = "slps_034.24"
+    config["myimg"] = "build/slps_034.24"
+    config["mapfile"] = "build/azumanga.map"
     config["source_directories"] = ["."]
     # config["show_line_numbers_default"] = True
     # config["arch"] = "mips"
